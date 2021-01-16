8ee0ca9458e4b542079901f0874e0fcacbdb2823
Arpit Shah <arpit.shah1@gmail.com>
2021-01-16 13:07:57 +1300

    Implemented Rest Server and end points for 1) Getting Robot info - Returns JSON object with all Robot info 2) Creating Robot - Create Robot and returns JSON object with all Robot info 3) Deleting Robot - Deletes Robot with provided ID 4) Making Robot move - Moves Robot with provided ID and given instructions 5) Queing Robot's current state - Provides current state of the Robot in JSON format

--- main.go
+++ main.go
@@ -9,40 +9,200 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
-var robots []Robot
+// Robots Fake Database slice
+var Robots []Robot
 
-// Models
+// IDCounter counter
+var IDCounter uint64
+
+// Robot Model
 type Robot struct {
-	TaskID     string      `json:"taskid"`
+	ID         uint64      `json:"id"`
 	RobotState *RobotState `json:"robotstate"`
 }
 
-// Models
+// RobotState Model
 type RobotState struct {
-	X        uint `json:"x"`
-	Y        uint `json:"y"`
-	HasCrate bool `json:"hascrate"`
+	X        uint64 `json:"x"`
+	Y        uint64 `json:"y"`
+	HasCrate bool   `json:"hascrate"`
+}
+
+/*
+	POST: localhost:8080/create will create robot with (X,Y) location to (0,0)
+	POST: localhost:8080/create?X=1&Y=1 will create robot with (X,Y) location to (1, 1)
+*/
+func createRobot(w http.ResponseWriter, r *http.Request) {
+
+	var xPosuint uint64
+	var yPosuint uint64
+
+	// Returns a url.Values, which is a map[string][]string
+	vals := r.URL.Query()
+
+	// Get Query Parameters
+	xPos, ok := vals["X"]
+	if ok {
+		// Convert query parameters to uInt64
+		xPosuint, _ = strconv.ParseUint(xPos[0], 10, 64)
+		if xPosuint > 10 {
+			xPosuint = 10
+		}
+	}
+	yPos, ok := vals["Y"]
+	if ok {
+		// Convert query parameters to uInt64
+		yPosuint, _ = strconv.ParseUint(yPos[0], 10, 64)
+		if yPosuint > 10 {
+			yPosuint = 10
+		}
+	}
+
+	// Create RobotState
+	state := new(RobotState)
+	state.X = xPosuint
+	state.Y = yPosuint
+	state.HasCrate = false
+
+	// Create Robot
+	robot := new(Robot)
+	robot.ID = IDCounter
+	IDCounter++
+	robot.RobotState = state
+
+	// Add Robot to databse
+	Robots = append(Robots, *robot)
+
+	// Return created Robot
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(Robots)
 }
 
 // Get Robot Info
 func getRobots(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(robots)
+	json.NewEncoder(w).Encode(Robots)
+}
+
+// Delete Robot
+func deleteRobot(w http.ResponseWriter, r *http.Request) {
+
+	robotID := mux.Vars(r)["id"]
+	// Convert to uInt64
+	robotIDuint, _ := strconv.ParseUint(robotID, 10, 64)
+
+	for i, singleRobot := range Robots {
+		if singleRobot.ID == robotIDuint {
+			Robots = append(Robots[:i], Robots[i+1:]...)
+			fmt.Fprintf(w, "The robot with ID %v has been deleted successfully", robotID)
+		}
+	}
+}
+
+// Get Robot Current State
+func getRobotState(w http.ResponseWriter, r *http.Request) {
+
+	robotID := mux.Vars(r)["id"]
+	// Convert to uInt64
+	robotIDuint, _ := strconv.ParseUint(robotID, 10, 64)
+
+	for i, singleRobot := range Robots {
+		if singleRobot.ID == robotIDuint {
+			singleRobotState := Robots[i].RobotState
+			// Return created Robot
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(singleRobotState)
+		}
+	}
+}
+
+// Move Robot
+func moveRobot(w http.ResponseWriter, r *http.Request) {
+
+	robotID := mux.Vars(r)["id"]
+	command := mux.Vars(r)["command"]
+	fmt.Println(robotID)
+	fmt.Println(command)
+
+	// Split strings using white space
+	actions := strings.Fields(command)
+
+	// Validate if its valid command
+	for i := 0; i < len(actions); i++ {
+		if len(actions[i]) != 1 {
+			fmt.Fprintf(w, "Invalid Command Length: %v", actions[i])
+		}
+		if actions[i] != "N" && actions[i] != "E" && actions[i] != "S" && actions[i] != "W" {
+			fmt.Fprintf(w, "Invalid Command: %v", actions[i])
+		}
+		fmt.Println(actions[i])
+	}
+
+	// Convert to uInt64
+	robotIDuint, _ := strconv.ParseUint(robotID, 10, 64)
+
+	for i, singleRobot := range Robots {
+		if singleRobot.ID == robotIDuint {
+			singleRobotState := Robots[i].RobotState
+
+			for i := 0; i < len(actions); i++ {
+				if actions[i] == "N" && singleRobotState.Y != 9 {
+					singleRobotState.Y++
+					fmt.Println("Travelling North")
+				}
+				if actions[i] == "S" && singleRobotState.Y != 0 {
+					singleRobotState.Y--
+					fmt.Println("Travelling South")
+				}
+				if actions[i] == "E" && singleRobotState.X != 9 {
+					singleRobotState.X++
+					fmt.Println("Travelling East")
+				}
+				if actions[i] == "W" && singleRobotState.X != 0 {
+					singleRobotState.X--
+					fmt.Println("Travelling East")
+				}
+
+			}
+
+			// Return created Robot
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(singleRobotState)
+		}
+	}
+}
+
+// Home Page
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!"+
+		"\nFollowing end points can be used"+
+		"\nlocalhost:8080/robots : will return all robots information"+
+		"\nlocalhost:8080/create : will create new robot with (0,0) location and return all robots information"+
+		"\nlocalhost:8080/create?X=1&Y=1 : will create new robot with (1,1) location and return all robots information"+
+		"\nlocalhost:8080/delete/1 : will delete robot with id=1 (if present)"+
+		"\nlocalhost:8080/state/1 : will return json object with current state of robot with id=1 (if present)"+
+		"\nlocalhost:8080/move/1/N N E : will move robot with id=1 two steps in north direction and one step in E direction. Robot will be in (1, 2) position")
 }
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Welcome to Robot SDK")
 
 	// Initialise router
-	r := mux.NewRouter()
+	r := mux.NewRouter().StrictSlash(true)
 
-	robots = append(robots, Robot{TaskID: "1", RobotState: &RobotState{X: 0, Y: 0, HasCrate: true}})
 	// Route Handlers / Endpoints
-	r.HandleFunc("/api/wh/robot", getRobots).Methods("GET")
+	r.HandleFunc("/", homeLink)
+	r.HandleFunc("/robots", getRobots).Methods("GET")
+	r.HandleFunc("/create", createRobot).Methods("POST")
+	r.HandleFunc("/delete/{id}", deleteRobot).Methods("POST")
+	r.HandleFunc("/state/{id}", getRobotState).Methods("GET")
+	r.HandleFunc("/move/{id}/{command}", moveRobot).Methods("POST")
 
 	// Start Server
 	log.Fatal(http.ListenAndServe(":8080", r))
